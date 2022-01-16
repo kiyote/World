ff8488c5bfd7a3d5b3c51c097dd9770a09c0b30e
Todd Lang <todd.lang@kiyote.ca>
2022-01-16 12:34:54 -0500

    Attempting to create separate prod path through deploy workflow

---
+++ terraform/environments/prod/backend.tf
@@ -0,0 +1,16 @@
+
+provider "aws" {
+	region = "ca-central-1"
+
+	assume_role = {
+		role_arn = "arn:aws:iam::860568434255:role/terraform_world_prod"
+	}
+}
+
+terraform {
+	backend "s3" {
+		bucket = "terraformstate6709d8b42a0c40a6be48b840ec9b12ff"
+		key    = "prod/world"
+		region = "ca-central-1"
+	}
+}
