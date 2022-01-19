733f6e214de32a53a79be47b07a8f325ef4b2de8
Todd Lang <todd.lang@d2l.com>
2022-01-19 09:16:57 -0500

    More assume_role tweaking

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -13,9 +13,6 @@ terraform {
     dynamodb_table = "terraformstate6709d8b42a0c40a6be48b840ec9b12ff"
     key    = "prod/world"
     region = "ca-central-1"
-  }
-
-  assume_role {
     role_arn = "arn:aws:iam::860568434255:role/terraform_world_dev"
   }
 }
