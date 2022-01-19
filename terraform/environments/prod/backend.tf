1f9b7d62c914ca458c8dc2b62a1d90efb131624d
Todd Lang <todd.lang@d2l.com>
2022-01-19 09:35:33 -0500

    More role definition twiddling

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "ca-central-1"
 
   assume_role {
-    role_arn = "arn:aws:iam::860568434255:role/terraform_world_dev"
+    role_arn = "arn:aws:iam::860568434255:role/terraform_world_prod"
   }
 }
 
@@ -13,5 +13,6 @@ terraform {
     dynamodb_table = "terraformstate6709d8b42a0c40a6be48b840ec9b12ff"
     key    = "prod/world"
     region = "ca-central-1"
+    role_arn = "arn:aws:iam::860568434255:role/terraform_world_prod"
   }
 }
