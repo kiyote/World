526c14dff4c8cdc3c5898540bcfbfa289085d701
Todd Lang <todd.lang@kiyote.ca>
2022-01-22 08:06:29 -0500

    Fixing terraform object_prefix constraints

--- terraform/common/_inputs.tf
+++ terraform/common/_inputs.tf
@@ -2,7 +2,7 @@
 variable "object_prefix" {
   type = string
   validation {
-    condition = contains(["dev", "prod"], var.object_prefix)
-    error_message = "The object prefix must be set to `dev` or `prod`."
+    condition = contains(["world.dev.", "world.prod."], var.object_prefix)
+    error_message = "The object prefix must be set to `world.dev.` or `world.prod.`."
   }
 }
