af074ee3e6b978688dfd2609aac5f9975ec0d44a
Todd Lang <todd.lang@d2l.com>
2022-02-15 10:49:04 -0500

    Settling on open simplex noise (#34)
    * Settling on open simplex noise
    
    * Commenting out image generation test

--- test/Server/Service/WorldBuilder/Server.Service.WorldBuilders/Noises/OpenSimplexTests.cs
+++ test/Server/Service/WorldBuilder/Server.Service.WorldBuilders/Noises/OpenSimplexTests.cs
@@ -19,8 +19,11 @@ public class OpenSimplexNoiseTests {
 		int width = 512;
 		int height = 512;
 
+		FastRandom random = new FastRandom();
+		long seed = random.NextLong();
+
 		using var img = new Image<Rgb24>( width, height );
-		float[,] noise = _noise.Generate( height, width );
+		float[,] noise = _noise.Generate( seed, height, width, 2.0f );
 		for( int r = 0; r < height; r++ ) {
 			for( int c = 0; c < width; c++ ) {
 				float value = noise[c, r];
