6deeb6bfdfb29a2ff2485137a33746395414c391
Todd Lang <todd.lang@d2l.com>
2022-02-17 10:02:44 -0500

    Adding object names to module output

---
+++ terraform/modules/files-manager/_outputs.tf
@@ -0,0 +1,3 @@
+variable "files_bucket_name" {
+  value = aws_s3_bucket.files_bucket.name
+}
---
+++ terraform/modules/shared/_outputs.tf
@@ -0,0 +1,3 @@
+variable "shared_table_name" {
+  value = aws_dynamodb_table.shared_dynamodb_table.name
+}
