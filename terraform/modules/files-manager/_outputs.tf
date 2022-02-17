00a29f838430c35c168f20cc34c9d1b832950dad
Todd Lang <todd.lang@d2l.com>
2022-02-17 10:04:58 -0500

    Fixing the declaration of the output

--- terraform/modules/files-manager/_outputs.tf
+++ terraform/modules/files-manager/_outputs.tf
@@ -1,3 +1,4 @@
-variable "files_bucket_name" {
+output "files_bucket_name" {
   value = aws_s3_bucket.files_bucket.name
+  description = "The name of the files bucket."
 }
--- terraform/modules/shared/_outputs.tf
+++ terraform/modules/shared/_outputs.tf
@@ -1,3 +1,4 @@
-variable "shared_table_name" {
+output "shared_table_name" {
   value = aws_dynamodb_table.shared_dynamodb_table.name
+  description = "The name of the shared dynamodb table."
 }
