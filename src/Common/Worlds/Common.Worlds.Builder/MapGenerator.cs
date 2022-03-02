e924cece68ed5a9845236f7a8fa371c331753f40
Todd Lang <todd.lang@d2l.com>
2022-03-02 15:39:48 -0500

    Split out mask generator

--- src/Common/Worlds/Common.Worlds.Builder/MapGenerator.cs
+++ src/Common/Worlds/Common.Worlds.Builder/MapGenerator.cs
@@ -8,6 +8,7 @@ internal class MapGenerator : IMapGenerator {
 	private readonly IRandom _random;
 	private readonly INoiseProvider _noiseProvider;
 	private readonly INoiseOperator _noiseOperator;
+	private readonly INoiseMaskGenerator _noiseMaskGenerator;
 
 	public const float Frequency = 6.00f;
 
@@ -25,11 +26,13 @@ internal class MapGenerator : IMapGenerator {
 	public MapGenerator(
 		IRandom random,
 		INoiseProvider noiseProvider,
-		INoiseOperator noiseOperator
+		INoiseOperator noiseOperator,
+		INoiseMaskGenerator noiseMaskGenerator
 	) {
 		_random = random;
 		_noiseProvider = noiseProvider;
 		_noiseOperator = noiseOperator;
+		_noiseMaskGenerator = noiseMaskGenerator;
 	}
 
 	TileTerrain[,] IMapGenerator.GenerateTerrain(
@@ -46,7 +49,7 @@ internal class MapGenerator : IMapGenerator {
 	) {
 		int columns = size.Columns;
 		int rows = size.Rows;
-		float[,] terrainMask = GenerateTerrainMask( size );
+		float[,] terrainMask = _noiseMaskGenerator.Circle( size );
 
 		_random.Reinitialise( (int)seed );
 		float[,] raw = _noiseProvider.Random( seed, rows, columns, 2.0f );
@@ -90,36 +93,5 @@ internal class MapGenerator : IMapGenerator {
 		}
 
 		return terrain;
-	}
-
-	private float[,] GenerateTerrainMask(
-		Size size
-	) {
-		float cx = (float)size.Columns / 2.0f;
-		float cy = (float)size.Rows / 2.0f;
-		float[,] gradient = new float[size.Columns, size.Rows];
-		float maxValue = float.MinValue;
-		float minValue = float.MaxValue;
-		for( int r = 0; r < size.Rows; r++ ) {
-			for( int c = 0; c < size.Columns; c++ ) {
-				double realDistance = Math.Pow( cx - c, 2 ) + Math.Pow( cy - r, 2 );
-				float distance = (float)Math.Sqrt( realDistance );
-				if( distance > maxValue ) {
-					maxValue = distance;
-				}
-				if( distance < minValue ) {
-					minValue = distance;
-				}
-				gradient[c, r] = distance;
-			}
-		}
-
-		// Push the edge from the corner to the mid-point
-		float target = gradient[size.Columns / 2, 0];
-		gradient = _noiseOperator.Threshold( ref gradient, minValue, minValue, target, maxValue );
-		gradient = _noiseOperator.Normalize( ref gradient );
-		gradient = _noiseOperator.Invert( ref gradient );
-
-		return gradient;
 	}
 }
---
+++ src/Common/Worlds/Common.Worlds.Builder/Noises/SimpleNoiseMaskGenerator.cs
@@ -0,0 +1,44 @@
+﻿namespace Common.Worlds.Builder.Noises;
+
+internal sealed class SimpleNoiseMaskGenerator : INoiseMaskGenerator {
+
+	private readonly INoiseOperator _noiseOperator;
+
+	public SimpleNoiseMaskGenerator(
+		INoiseOperator noiseOperator
+	) {
+		_noiseOperator = noiseOperator;
+	}
+
+	public float[,] Circle(
+		Size size
+	) {
+		float cx = (float)size.Columns / 2.0f;
+		float cy = (float)size.Rows / 2.0f;
+		float[,] gradient = new float[size.Columns, size.Rows];
+		float maxValue = float.MinValue;
+		float minValue = float.MaxValue;
+		for( int r = 0; r < size.Rows; r++ ) {
+			for( int c = 0; c < size.Columns; c++ ) {
+				double realDistance = Math.Pow( cx - c, 2 ) + Math.Pow( cy - r, 2 );
+				float distance = (float)Math.Sqrt( realDistance );
+				if( distance > maxValue ) {
+					maxValue = distance;
+				}
+				if( distance < minValue ) {
+					minValue = distance;
+				}
+				gradient[c, r] = distance;
+			}
+		}
+
+		// Push the edge from the corner to the mid-point
+		float target = gradient[size.Columns / 2, 0];
+		gradient = _noiseOperator.Threshold( ref gradient, minValue, minValue, target, maxValue );
+		gradient = _noiseOperator.Normalize( ref gradient );
+		gradient = _noiseOperator.Invert( ref gradient );
+
+		return gradient;
+	}
+}
+
