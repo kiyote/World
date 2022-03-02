8e864287ed0b15dd688d0224fb38f8b5bd9fe8c2
Todd Lang <todd.lang@d2l.com>
2022-03-02 16:58:23 -0500

    Adding minimap renderer

---
+++ src/Common/Renderers/Common.Renderers.Minimap/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo( "Common.Renderers.Minimap.Tests" )]
+// Needed for Moq to work
+[assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
---
+++ test/Common/Renderers/Common.Renderers.Minimap.Tests/MinimapWorldRendererIntegrationTests.cs
@@ -0,0 +1,69 @@
+using Common.Files;
+using Common.Files.Manager.Resource;
+using Common.Worlds;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Server.Files.Manager.Disk;
+
+namespace Common.Renderers.Minimap.Tests;
+
+[TestFixture]
+public class MinimapWorldRendererIntegrationTests {
+
+	private IServiceScope _scope;
+	private IServiceProvider _provider;
+	private IWorldRenderer _renderer;
+	private string _folder;
+
+	[OneTimeSetUp]
+	public void OneTimeSetUp() {
+		_folder = Path.Combine( Path.GetTempPath(), Guid.NewGuid().ToString( "N" ) );
+		Directory.CreateDirectory( _folder );
+
+		var services = new ServiceCollection();
+		services.AddResourceFileManager();
+		services.AddDiskFileManager( _folder );
+		services.AddMinimapRenderer();
+
+		_provider = services.BuildServiceProvider();
+	}
+
+	[OneTimeTearDown]
+	public void OneTimeTearDown() {
+		Directory.Delete( _folder, true );
+	}
+
+	[SetUp]
+	public void SetUp() {
+		_scope = _provider.CreateScope();
+
+		_renderer = _provider.GetRequiredService<IWorldRenderer>();
+	}
+
+	[TearDown]
+	public void TearDown() {
+		_scope.Dispose();
+	}
+
+	[Test]
+	[Ignore( "Used to generate visual output for inspection." )]
+	public async Task RenderTerrain() {
+		IDiskFileManager diskFileManager = _provider.GetRequiredService<IDiskFileManager>();
+
+		Id<FileMetadata> fileId = new Id<FileMetadata>( "terrain.png" );
+		TileTerrain[,] tileTerrain = new TileTerrain[1000, 1000];
+
+		for( int r = 0; r < tileTerrain.GetLength( 0 ); r++ ) {
+			for( int c = 0; c < tileTerrain.GetLength( 1 ); c++ ) {
+				tileTerrain[c, r] = TileTerrain.Grass;
+			}
+		}
+
+		await _renderer.RenderAtlasToAsync(
+			diskFileManager,
+			fileId,
+			tileTerrain,
+			CancellationToken.None
+		).ConfigureAwait( false );
+	}
+}
