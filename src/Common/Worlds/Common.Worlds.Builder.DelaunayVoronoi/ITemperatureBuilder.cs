4ae83e5af7a41d975479cf037e6bda86e586063e
Todd Lang <todd.lang@d2l.com>
2022-05-29 15:16:04 -0400

    Adding TemperatureBuilder

---
+++ src/Common/Worlds/Common.Worlds.Builder.DelaunayVoronoi/ITemperatureBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.Worlds.Builder.DelaunayVoronoi;
+
+internal interface ITemperatureBuilder {
+	Dictionary<Cell, int> Create(
+		Size size,
+		Voronoi fineVoronoi,
+		HashSet<Cell> fineLandforms,
+		HashSet<Cell> mountains,
+		HashSet<Cell> hills,
+		HashSet<Cell> oceans,
+		HashSet<Cell> lakes
+	);
+}
