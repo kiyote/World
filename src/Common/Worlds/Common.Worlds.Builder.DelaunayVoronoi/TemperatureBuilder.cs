fc99d1c11bdc781d827638a1c3db1df2ac2ad94a
Todd Lang <todd.lang@d2l.com>
2022-05-29 15:29:45 -0400

    Adding TemperatureBuilder (#106)

---
+++ src/Common/Worlds/Common.Worlds.Builder.DelaunayVoronoi/TemperatureBuilder.cs
@@ -0,0 +1,120 @@
+﻿using Common.Geometry;
+
+namespace Common.Worlds.Builder.DelaunayVoronoi;
+
+internal sealed class TemperatureBuilder : ITemperatureBuilder {
+
+	private readonly IGeometry _geometry;
+
+	public TemperatureBuilder(
+		IGeometry geometry
+	) {
+		_geometry = geometry;
+	}
+
+	Dictionary<Cell, int> ITemperatureBuilder.Create(
+		Size size,
+		Voronoi fineVoronoi,
+		HashSet<Cell> fineLandforms,
+		HashSet<Cell> mountains,
+		HashSet<Cell> hills,
+		HashSet<Cell> oceans,
+		HashSet<Cell> lakes
+	) {
+		int midLine = size.Rows / 2;
+
+		Dictionary<Cell, int> result = new Dictionary<Cell, int>();
+		foreach( Cell cell in fineVoronoi.Cells ) {
+			result[cell] = 0;
+		}
+
+		int maxValue = 0;
+		List<Cell> current = RenderEquator( midLine, size, fineVoronoi, result );
+		do {
+			current = HeatNeighbours( fineVoronoi, current, result, ref maxValue );
+		} while( current.Count > 0 );
+
+		foreach( KeyValuePair<Cell, int> entry in result ) {
+			result[entry.Key] = maxValue - entry.Value;
+		}
+
+		foreach( Cell land in fineLandforms ) {
+			result[land] = result[land] + (maxValue / 5);
+		}
+
+		foreach( Cell mountain in mountains ) {
+			result[mountain] = result[mountain] / 5;
+		}
+
+		foreach( Cell hill in hills ) {
+			result[hill] = result[hill] / 3;
+		}
+
+		return result;
+	}
+
+	private List<Cell> RenderEquator(
+		int midLine,
+		Size size,
+		Voronoi voronoi,
+		Dictionary<Cell, int> temperatureMap
+	) {
+		List<Cell> result = new List<Cell>();
+		_geometry.RasterizeLine(
+			new Point( 0, midLine ),
+			new Point( size.Columns, midLine ),
+			( int x, int y ) => {
+				Cell? cell = FindCell( voronoi, x, y );
+				if( cell is not null
+					&& temperatureMap[cell] == 0
+				) {
+					temperatureMap[cell] = 1;
+					result.Add( cell );
+				}
+			}
+		);
+		return result;
+	}
+
+	private static List<Cell> HeatNeighbours(
+		Voronoi fineVoronoi,
+		List<Cell> current,
+		Dictionary<Cell, int> result,
+		ref int maxValue
+	) {
+		List<Cell> neighbours = new List<Cell>();
+		foreach( Cell source in current ) {
+			foreach( Cell neighbour in fineVoronoi.Neighbours[source].Where( c => !c.IsOpen ) ) {
+				if( result[neighbour] == 0 ) {
+					if( neighbour.Center.Y < source.Center.Y
+						|| neighbour.Center.Y > source.Center.Y
+					) {
+						result[neighbour] = result[source] + 1;
+
+						if( result[neighbour] > maxValue ) {
+							maxValue = result[neighbour];
+						}
+
+					} else {
+						result[neighbour] = result[source];
+					}
+					neighbours.Add( neighbour );
+				}
+			}
+		}
+		return neighbours;
+	}
+
+	private Cell? FindCell(
+		Voronoi voronoi,
+		int x,
+		int y
+	) {
+		foreach( Cell cell in voronoi.Cells ) {
+			if( _geometry.PointInPolygon( cell.Points, x, y ) ) {
+				return cell;
+			}
+		}
+		return null;
+	}
+}
---
+++ test/Common/Worlds/Common.Worlds.Builder.DelaunayVoronoi.Tests/TemperatureBuilderIntegrationTests.cs
@@ -0,0 +1,109 @@
+﻿using Common.Buffers;
+using Common.Geometry;
+using Common.Geometry.DelaunayVoronoi;
+
+namespace Common.Worlds.Builder.DelaunayVoronoi.Tests;
+
+[TestFixture]
+internal sealed class TemperatureBuilderIntegrationTests {
+
+	private ILandformBuilder _landformBuilder;
+	private IBufferFactory _bufferFactory;
+	private IGeometry _geometry;
+	private ISaltwaterBuilder _saltwaterBuilder;
+	private IFreshwaterBuilder _freshwaterBuilder;
+	private IMountainsBuilder _mountainsBuilder;
+	private IHillsBuilder _hillsBuilder;
+	private TemperatureBuilder _builder;
+
+	private IServiceProvider _provider;
+	private IServiceScope _scope;
+	private string _folder;
+
+	[OneTimeSetUp]
+	public void OneTimeSetUp() {
+		string rootPath = Path.Combine( Path.GetTempPath(), "world" );
+		_folder = Path.Combine( rootPath, nameof( TemperatureBuilderIntegrationTests ) );
+		Directory.CreateDirectory( _folder );
+		var services = new ServiceCollection();
+		services.AddCommonCore();
+		services.AddCommonBuffers();
+		services.AddCommonGeometry();
+		services.AddCommonWorlds();
+
+		services.AddCommonWorldsBuilderDelaunayVoronoi();
+
+		_provider = services.BuildServiceProvider();
+
+	}
+
+	[OneTimeTearDown]
+	public void OneTimeTearDown() {
+		Directory.Delete( _folder, true );
+	}
+
+	[SetUp]
+	public void SetUp() {
+		_scope = _provider.CreateScope();
+
+		_bufferFactory = _scope.ServiceProvider.GetRequiredService<IBufferFactory>();
+		_geometry = _scope.ServiceProvider.GetRequiredService<IGeometry>();
+		_landformBuilder = _scope.ServiceProvider.GetRequiredService<ILandformBuilder>();
+		_mountainsBuilder = _scope.ServiceProvider.GetRequiredService<IMountainsBuilder>();
+		_hillsBuilder = _scope.ServiceProvider.GetRequiredService<IHillsBuilder>();
+		_saltwaterBuilder = _scope.ServiceProvider.GetRequiredService<ISaltwaterBuilder>();
+		_freshwaterBuilder = _scope.ServiceProvider.GetRequiredService<IFreshwaterBuilder>();
+
+		_builder = new TemperatureBuilder(
+			_geometry
+		);
+	}
+
+	[TearDown]
+	public void TearDown() {
+		_scope.Dispose();
+	}
+
+	[Test]
+	[Ignore( "Used to visualize output for inspection." )]
+	public async Task Visualize() {
+		Size size = new Size( 1000, 1000 );
+		HashSet<Cell> fineLandforms = _landformBuilder.Create( size, out Voronoi fineVoronoi );
+		HashSet<Cell> mountains = _mountainsBuilder.Create( size, fineVoronoi, fineLandforms );
+		HashSet<Cell> hills = _hillsBuilder.Create( fineVoronoi, fineLandforms, mountains );
+		HashSet<Cell> oceans = _saltwaterBuilder.Create( size, fineVoronoi, fineLandforms );
+		HashSet<Cell> lakes = _freshwaterBuilder.Create( fineVoronoi, fineLandforms, oceans );
+		Dictionary<Cell, int> temperatures = ( _builder as ITemperatureBuilder ).Create(
+			size,
+			fineVoronoi,
+			fineLandforms,
+			mountains,
+			hills,
+			oceans,
+			lakes
+		);
+
+		IBuffer<float> buffer = _bufferFactory.Create<float>( size );
+
+		int maxValue = temperatures.Values.Max();
+		foreach( Cell cell in temperatures.Keys ) {
+			float temp = (float)temperatures[cell] / (float)maxValue;
+			_geometry.RasterizePolygon( cell.Points, ( int x, int y ) => {
+				if( x >= 0 && x < size.Columns && y >= 0 && y < size.Rows ) {
+					buffer[x, y] = temp;
+				}
+			} );
+		}
+
+		foreach( Edge edge in fineVoronoi.Edges ) {
+			_geometry.RasterizeLine( edge.A, edge.B, ( int x, int y ) => {
+				if( x >= 0 && x < size.Columns && y >= 0 && y < size.Rows ) {
+					buffer[x, y] = 0.2f;
+				}
+			} );
+		}
+
+		IBufferWriter<float> writer = new ImageBufferWriter( Path.Combine( _folder, "temperature.png" ) );
+		await writer.WriteAsync( buffer );
+	}
+}
